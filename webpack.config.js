d854d258f6186e23b075505888cde3a49a03bb68
GusBaamonde <gusvodesign@gmail.com>
2022-02-03 17:41:45 -0300

    chore: add configuration environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require("webpack");
+const DotenvFlow = require('dotenv-flow-webpack');
+const Dotenv = require('dotenv')
 
 const rulesStyles = {
     test: /\.(sa|sc|c)ss$/,
@@ -12,25 +15,35 @@ const rulesStyles = {
     ],
 }
 
-/*const rulesBabel = {
-    test: /\.m?js$/,
-    exclude: /(node_modules|bower_components)/,
-    use: {
-        loader: 'babel-loader',
-        options: {
-            presets: ['@babel/preset-env']
-        }
-    }
-}*/
-
 module.exports = function (env) {
-    return {
+    Dotenv.config({path: './.env.' + env.NODE_ENV});
+    const config = {
         output: {
             path: path.resolve(__dirname, 'build')
         },
-        plugins: [new MiniCssExtractPlugin()],
+        plugins: [
+            new MiniCssExtractPlugin(),
+            new webpack.ProvidePlugin({
+                process: 'process/browser',
+            }),
+            new DotenvFlow({
+                node_env: env.NODE_ENV ? env.NODE_ENV : 'development'
+            })],
         module: {
             rules: [rulesStyles/*, rulesBabel*/]
         }
     }
+    if (env.NODE_ENV === 'development') {
+        config.mode = 'development'
+        config.devtool = 'inline-source-map'
+        config.devServer = {
+            historyApiFallback: true,
+            static: './build',
+            port: 3500,
+        }
+        config.watchOptions = {
+            ignored: [path.resolve(__dirname, 'src'), './node_modules']
+        }
+    }
+    return config
 }
